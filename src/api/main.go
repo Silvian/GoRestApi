24e64f94a99cce9606fac05392771bb53b22277f
silvian.dragan <silvian.dragan@glintpay.com>
2018-04-16 15:39:38 +0100

    reformatted code style

--- src/api/main.go
+++ src/api/main.go
@@ -1,25 +1,26 @@
 package api
 
 import (
-  "log"
-  "net/http"
-  "github.com/gorilla/mux"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-  // Init router
-  router := mux.NewRouter()
+	// Init router
+	router := mux.NewRouter()
 
-  // Mock data
-  loadMockData()
+	// Mock data
+	loadMockData()
 
-  // Route handlers / Endpoints
-  router.HandleFunc("/api/books", getBooks).Methods("GET")
-  router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-  router.HandleFunc("/api/books", createBook).Methods("POST")
-  router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-  router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	// Route handlers / Endpoints
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-  server := http.ListenAndServe(":8080", router)
-  log.Fatal(server)
+	server := http.ListenAndServe(":8080", router)
+	log.Fatal(server)
 }
--- src/api/models.go
+++ src/api/models.go
@@ -2,14 +2,14 @@ package api
 
 // Book Struct (Model)
 type Book struct {
-  ID        string `json:"id"`
-  Isbn      string `json:"isbn"`
-  Title     string `json:"title"`
-  Author    *Author `json:"author"`
+	ID        string `json:"id"`
+	Isbn      string `json:"isbn"`
+	Title     string `json:"title"`
+	Author    *Author `json:"author"`
 }
 
 // Author Struct (Model)
 type Author struct {
-  Firstname   string `json:"firstname"`
-  Lastname    string `json:"lastname"`
+	Firstname   string `json:"firstname"`
+	Lastname    string `json:"lastname"`
 }
